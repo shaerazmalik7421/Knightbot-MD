5332c7c7ed9e905ee2a7c7d940724d947ab505d6
shaerazmalik7421 <165659336+shaerazmalik7421@users.noreply.github.com>
2025-02-04 22:36:42 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'ShaerazMalik',
+  author: 'Malik',
+  botName: "ShaerazMalik",
+  botOwner: 'ShaerazMalik', // Your name
+  ownerNumber: '994405995665', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
